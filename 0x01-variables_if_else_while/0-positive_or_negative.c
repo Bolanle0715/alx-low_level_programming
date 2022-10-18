568e013fb93a98315684b6fb4315abd59ba4baef
Bolanle0715 <bolanlebukolaojelabi@gmail.com>
2022-10-18 02:10:25 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,16 +6,17 @@
  * main - main function to generate a random number
  *
  * Return: Always 0 (success)
+ *
 */
 
 int main(void)
 {
 	int n;
 
-	srand (time(0));
-	n = rand() - RAND_MAX /2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
