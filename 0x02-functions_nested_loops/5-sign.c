c3ef1484a41d2acc6091f860dda2e18359b35db5
Bolanle0715 <bolanlebukolaojelabi@gmail.com>
2022-10-19 13:12:12 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,14 +3,10 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 of the number is zero
- * -1 if number is less than zero
+ * Return: 1 if number is greater than zero, 1 if not
 */
 int print_sign(int n)
 {
-	int num;
-
 	if (n > o)
 	{
 		_putchar('+');
