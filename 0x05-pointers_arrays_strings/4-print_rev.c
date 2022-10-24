a68c5e02ec01df69f7f7b8019c911c64cc827d39
Bolanle0715 <bolanlebukolaojelabi@gmail.com>
2022-10-24 13:59:51 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\n')
+	while (s[i] != '\0')
 		i++;
 	for (i = i - 1; i >= 0; i--)
-		_putch6ar(s[i]);
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
